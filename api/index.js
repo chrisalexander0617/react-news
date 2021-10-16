5d20a8f78870313717cf445a22b8f63beb71c23b
Chris <44784345+chrisalexander0617@users.noreply.github.com>
2021-10-15 20:42:21 -0400

    Added express statci

--- api/index.js
+++ api/index.js
@@ -9,6 +9,7 @@ const app = express();
 
 app.use(cors());
 app.use(express.json());
+app.use(express.static('public'))
 
 app.get('/', (rq, rs) => {
     rs.status(200).json([ {message:'Success'} ])
